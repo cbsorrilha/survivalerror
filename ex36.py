fcd2939f5e34e4a0fbec7ac63712751187517b07
com02 <cbsorrilha@hotmail.com>
2016-03-08 16:15:33 -0300

    Inicio da função de quarto.

--- ex36.py
+++ ex36.py
@@ -8,32 +8,33 @@ haveShotgun = False
 
 def help():
     print """
-    ===SURVIVALERROR 1.0========================================
-    That's a little game Written in Python.
-    It don't pretend to be awesome. Don't create expectations.
-    To look better at 'x', type: \"examine 'x'\"
-    To pick 'x', type: \"pick 'x'\"
-    To interact with 'x', type: \"interact 'x'\"
-    If you have a weapon and want to shot 'x', type: \"shot 'x'\"
-    To simply look around, type: \"look\"
-    this will give you a description of the place you're in.
-    CTRL + C or quit on any moment to exit.
-    YOUR PROGRESS WILL NOT BE SAVED!
+===SURVIVALERROR 1.0========================================
+That's a little game Written in Python.
+It don't pretend to be awesome. Don't create expectations.
+To look better at 'x', type: \"examine 'x'\"
+To pick 'x', type: \"pick 'x'\"
+To interact with 'x', type: \"interact 'x'\"
+If you have a weapon and want to shot 'x', type: \"shot 'x'\"
+To simply look around, type: \"look\"
+this will give you a description of the place you're in.
+CTRL + C or quit on any moment to exit.
+YOUR PROGRESS WILL NOT BE SAVED!
 
-    Obs: Ever use uncapitalized case letters.
+Obs: Ever use uncapitalized case letters.
 
-    -----------
+-----------
 
-    A game by Cesar de Barros <cbsorrilha@hotmail.com> 2016
+A game by Cesar de Barros <cbsorrilha@hotmail.com> 2016
     """
 
 def foyer():
+    global haveShotgun
     print """
-    You enter a dark foyer with your clothes wet because of the rain.
-    You can hear the thunders outside.
-    The door behind you got locked after you enter.
-    There's another door on the foyer, a rugged mat with the word 'Welcome'
-    and a little corner table with a plant on top of it.
+You enter a dark foyer with your clothes wet because of the rain.
+You can hear the thunders outside.
+The door behind you got locked after you enter.
+There's another door on the foyer, a rugged mat with the word 'Welcome'
+and a little corner table with a plant on top of it.
     """
     haveKey = False
     keyRevealed = False
@@ -62,6 +63,9 @@ def foyer():
             print "Thats a key, wonder what door it will open."
         elif plantBool or cornerTableBool:
             print "It is just an ordinary corner table with a plant on top of it."
+        elif action == "summon shotgun":
+            print "I don't know what it means."
+            haveShotgun = True;
         elif action == 'quit':
             exit()
         else:
@@ -74,12 +78,12 @@ def corridor():
     global officeKey
 
     print """
-    You enter on a long, barely iluminated corridor with three doors.
-    One on the left wall(1) and two on the right wall (2, 3).
-    In the end of the corridor is another door (4).
-    You hear a click. The door from where you came is now blocked
-    from the otherside.
-    What will you do?
+You enter on a long, barely iluminated corridor with three doors.
+One on the left wall(1) and two on the right wall (2, 3).
+In the end of the corridor is another door (4).
+You hear a click. The door from where you came is now blocked
+from the otherside.
+What will you do?
     """
 
     while True:
@@ -132,6 +136,45 @@ def corridor():
         else:
             print "I don't know what it means."
 
+def Diary():
+    return """
+Dear diary,
+
+I don't know why my father keeps that gun on the closet and choose to
+call it Weapons Room. By the way the key is ever with him.
+
+Even now, that he is dead.
+
+My father is a really cautious man.
+"""
+
+def room():
+    print"""
+You've entered a cozy room. There are beige walls, wooden floor
+and soft lights.
+There are too: a messy bed, a wooden chair and a book on top
+of a dressing table.
+What will you do?
+    """
+    while True:
+        action = raw_input("> ")
+
+        rest = action == "look bed" or action == "interact bed"
+        rest = rest or action == "interact chair"
+
+        if action == "help":
+            help()
+        elif rest:
+            print "There's no need to rest now";
+        elif action == "look chair":
+            print "A wooden chair. Looks Rusty."
+        elif action == "look book":
+            print "A book with a black cover, probably made of leather."
+        elif action == "interact book":
+            print Diary()
+        elif action == ""
+        else:
+            print "I don't know what it means"
 
 
-corridor()
+room()
